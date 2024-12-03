9f755d7c1cf5c14fb18562de37479a623ab0619a
Stefan Strömer <stefan.stroemer@ait.ac.at>
2024-12-03 12:44:08 +0100

    update to 06c

--- addons/Battery.jl
+++ addons/Battery.jl
@@ -29,4 +29,81 @@ function construct_variables!(model::JuMP.Model, config::Dict)
     return true
 end
 
+function construct_constraints!(model::JuMP.Model, config::Dict)
+    batteries = config["batteries"]
+    T = get_T(model)
+
+    for battery in batteries
+        state_bounds(battery)
+        degradation(battery, config["degradation_perc_per_cycle"])
+    end
+
+    return true
+end
+
+function construct_objective!(model::JuMP.Model, config::Dict)
+    batteries = config["batteries"]
+
+    for battery in batteries
+        # Total degradation is the difference between the initial and the final useable capacity.
+        battery.exp.total_degradation = access(battery.storage.state_ub) - battery.var.useable_capacity[end]
+
+        # Degradation cost is the total degradation times the cost per kWh degraded.
+        battery.obj.degradation_cost = battery.exp.total_degradation * battery.config["cost_per_kwh_degraded"]
+
+        # Add the degradation cost to the overall default objective.
+        IESopt.add_term_to_objective!(model, "total_cost", battery.obj.degradation_cost)
+    end
+
+    return true
+end
+
+function state_bounds(battery)
+    T = get_T(battery.model)
+    # An alternative would be to pass the model, by changing the function to:
+    # `function state_bounds(model, battery)`
+
+    var_uc = battery.var.useable_capacity
+    var_state = battery.storage.var.state
+
+    battery.con.state_lower_bound = JuMP.@constraint(
+        battery.model,
+        [t = T],
+        0.1 * var_uc[t] <= var_state[t],
+        base_name = make_base_name(battery, "state_lower_bound"),
+        container = Array,
+    )
+
+    return battery.con.state_upper_bound = JuMP.@constraint(
+        battery.model,
+        [t = T],
+        var_state[t] <= 0.9 * var_uc[t],
+        base_name = make_base_name(battery, "state_upper_bound"),
+        container = Array,
+    )
+end
+
+function degradation(battery, degradation_perc_per_cycle)
+    T = get_T(battery.model)
+
+    var_uc = battery.var.useable_capacity
+    total_capacity = access(battery.storage.state_ub)
+
+    utilization = (battery.charging.var.flow .+ battery.discharging.var.flow) ./ 2.0
+
+    return battery.con.degradation = JuMP.@constraint(
+        battery.model,
+        [t = T],
+        var_uc[t] == (
+            if t == T[1]
+                total_capacity
+            else
+                var_uc[t - 1] - utilization[t - 1] * degradation_perc_per_cycle
+            end
+        ),
+        base_name = make_base_name(battery, "degradation"),
+        container = Array,
+    )
+end
+
 end
--- main.py
+++ main.py
@@ -4,7 +4,47 @@ import numpy as np
 from plotly.subplots import make_subplots
 
 
-model = iesopt.Model("battery.iesopt.yaml", skip_validation=True)
-model.generate()
+options = [
+    dict(battery_power=5, battery_hours=2),
+    dict(battery_power=10, battery_hours=1),
+    dict(battery_power=1, battery_hours=10),
+    dict(battery_power=2, battery_hours=5),
+]
 
-model.get_component("battery").var.useable_capacity
+for opt in options:
+    model = iesopt.run("battery.iesopt.yaml", parameters=opt, skip_validation=True)
+    discharging = sum(model.results.components["battery.discharging"].var.flow)
+    energy_bought = sum(model.results.components["energy_supplier"].exp.value) / 1e3
+    print(f"{opt}")
+    print(f"      ╰──>  {model.objective_value:.1f} EUR      {discharging:.1f} kWh  |  (buy: {energy_bought:.1f} MWh)\n")
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# BEFORE
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# {'battery_power': 5, 'battery_hours': 2}
+#       ╰──>  2746.0 EUR  |  1322.2 kWh  |  (buy: 17.30 MWh)
+
+# {'battery_power': 10, 'battery_hours': 1}
+#       ╰──>  2746.0 EUR  |  1322.2 kWh  |  (buy: 17.30 MWh)
+
+# {'battery_power': 1, 'battery_hours': 10}
+#       ╰──>  2751.1 EUR  |  1106.1 kWh  |  (buy: 17.49 MWh)
+
+# {'battery_power': 2, 'battery_hours': 5}
+#       ╰──>  2746.1 EUR  |  1317.7 kWh  |  (buy: 17.30 MWh)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# NOW
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# {'battery_power': 5, 'battery_hours': 2}
+#       ╰──>  2745.9 EUR      1319.3 kWh  |  (buy: 17.3 MWh)
+
+# {'battery_power': 10, 'battery_hours': 1}
+#       ╰──>  2745.9 EUR      1319.3 kWh  |  (buy: 17.3 MWh)
+
+# {'battery_power': 1, 'battery_hours': 10}
+#       ╰──>  2751.0 EUR      1105.7 kWh  |  (buy: 17.5 MWh)
+
+# {'battery_power': 2, 'battery_hours': 5}
+#       ╰──>  2746.0 EUR      1314.9 kWh  |  (buy: 17.3 MWh)
