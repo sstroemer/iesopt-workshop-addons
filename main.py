e4dbc984de721458a97cc92ca72fde393ee33435
Stefan Strömer <stefan.stroemer@ait.ac.at>
2024-12-03 11:49:31 +0100

    update to 02

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 import iesopt
 import plotly.graph_objects as go
 import numpy as np
+from plotly.subplots import make_subplots
 
 
-# Run the model
 model = iesopt.run("battery.iesopt.yaml")
 
-# Extract generation and curtailment data
 generation = model.results.components["pv"].exp.value
 curtailment = model.get_component("pv").ub.value - generation
 
-# Group and sum data into 12 monthly values
 mg = np.array([sum(generation[i * 730 : (i + 1) * 730]) for i in range(12)])
 mc = np.array([sum(curtailment[i * 730 : (i + 1) * 730]) for i in range(12)])
+state_of_charge = model.results.components["grid"].var.state
+state_of_charge_week = state_of_charge[:168]
 
-# Create the stacked bar plot
-fig = go.Figure(
-    data=[
-        go.Bar(name="Generation", x=list(range(1, 13)), y=mg, marker_color="green"),
-        go.Bar(name="Curtailment", x=list(range(1, 13)), y=mc, marker_color="red"),
-    ]
-)
-
-# Update the layout to stack the bars
-fig.update_layout(
+make_subplots(
+    rows=1, cols=2, subplot_titles=("Monthly Gen. / Curt.", "Energy stored")
+).add_trace(
+    go.Bar(name="Generation", x=list(range(1, 13)), y=mg, marker_color="green"),
+    row=1,
+    col=1,
+).add_trace(
+    go.Bar(name="Curtailment", x=list(range(1, 13)), y=mc, marker_color="red"),
+    row=1,
+    col=1,
+).add_trace(
+    go.Scatter(
+        x=list(range(168)),
+        y=state_of_charge_week,
+        name="Battery",
+        mode="lines",
+        line_shape="hv",
+        line_color="black",
+    ),
+    row=1,
+    col=2,
+).update_layout(
     barmode="stack",
-    title="Monthly Gen. / Curt.",
+    title="Monthly Generation and Curt. / Battery content",
     xaxis_title="Month",
     yaxis_title="kWh",
-)
-
-# Show the plot
-fig.show()
+).update_xaxes(title_text="Time", row=1, col=2).update_yaxes(
+    title_text="kWh", row=1, col=2
+).show()
