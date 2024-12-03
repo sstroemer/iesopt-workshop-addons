56db6bd94c912da2eaf2583a1e2f891087ca9eb3
Stefan Strömer <stefan.stroemer@ait.ac.at>
2024-12-03 12:30:31 +0100

    update to 04

--- main.py
+++ main.py
@@ -4,43 +4,15 @@ import numpy as np
 from plotly.subplots import make_subplots
 
 
-model = iesopt.run("battery.iesopt.yaml")
-# model = iesopt.run("battery.iesopt.yaml", parameters=dict(battery_eff_rt=0.85))
+options = [
+    dict(battery_power=5, battery_hours=2),
+    dict(battery_power=10, battery_hours=1),
+    dict(battery_power=1, battery_hours=10),
+    dict(battery_power=2, battery_hours=5),
+]
 
-generation = model.results.components["pv"].exp.value
-curtailment = model.get_component("pv").ub.value - generation
-
-mg = np.array([sum(generation[i * 730 : (i + 1) * 730]) for i in range(12)])
-mc = np.array([sum(curtailment[i * 730 : (i + 1) * 730]) for i in range(12)])
-state_of_charge = model.results.components["grid"].var.state
-state_of_charge_week = state_of_charge[:168]
-
-make_subplots(
-    rows=1, cols=2, subplot_titles=("Monthly Gen. / Curt.", "Energy stored")
-).add_trace(
-    go.Bar(name="Generation", x=list(range(1, 13)), y=mg, marker_color="green"),
-    row=1,
-    col=1,
-).add_trace(
-    go.Bar(name="Curtailment", x=list(range(1, 13)), y=mc, marker_color="red"),
-    row=1,
-    col=1,
-).add_trace(
-    go.Scatter(
-        x=list(range(168)),
-        y=state_of_charge_week,
-        name="Battery",
-        mode="lines",
-        line_shape="hv",
-        line_color="black",
-    ),
-    row=1,
-    col=2,
-).update_layout(
-    barmode="stack",
-    title="Monthly Generation and Curt. / Battery content",
-    xaxis_title="Month",
-    yaxis_title="kWh",
-).update_xaxes(title_text="Time", row=1, col=2).update_yaxes(
-    title_text="kWh", row=1, col=2
-).show()
+for opt in options:
+    model = iesopt.run("battery.iesopt.yaml", parameters=opt, skip_validation=True)
+    discharging = sum(model.results.components["battery.discharging"].var.flow)
+    print(f"{opt}")
+    print(f"      ╰──>  {model.objective_value:.1f} EUR  |  {discharging:.1f} kWh\n")
