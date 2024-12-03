2454f7ae772fadadc26bd6f1bd03c343e8c7db53
Stefan Strömer <stefan.stroemer@ait.ac.at>
2024-12-03 12:38:54 +0100

    update to 06a

---
+++ addons/Battery.jl
@@ -0,0 +1,31 @@
+module IESoptAddon_Battery
+
+using IESopt
+import JuMP
+
+function initialize!(model::JuMP.Model, config::Dict)
+    config["batteries"] = get_components(model; tagged="SimpleBattery")
+
+    # All functions are expected to return `true` if everything went well.
+    return true
+end
+
+function construct_variables!(model::JuMP.Model, config::Dict)
+    batteries = config["batteries"]
+    T = get_T(model)
+
+    for battery in batteries
+        total_capacity = access(battery.storage.state_ub)
+        battery.var.useable_capacity = JuMP.@variable(
+            model,
+            [t = T],
+            lower_bound = 0,
+            upper_bound = total_capacity,
+            container = Array,  # This is important, since JuMP does not "see" that our "T" allows for arrays.
+        )
+    end
+
+    return true
+end
+
+end
--- main.py
+++ main.py
@@ -4,16 +4,7 @@ import numpy as np
 from plotly.subplots import make_subplots
 
 
-options = [
-    dict(battery_power=5, battery_hours=2),
-    dict(battery_power=10, battery_hours=1),
-    dict(battery_power=1, battery_hours=10),
-    dict(battery_power=2, battery_hours=5),
-]
+model = iesopt.Model("battery.iesopt.yaml", skip_validation=True)
+model.generate()
 
-for opt in options:
-    model = iesopt.run("battery.iesopt.yaml", parameters=opt, skip_validation=True)
-    discharging = sum(model.results.components["battery.discharging"].var.flow)
-    energy_bought = sum(model.results.components["energy_supplier"].exp.value) / 1e3
-    print(f"{opt}")
-    print(f"      ╰──>  {model.objective_value:.1f} EUR      {discharging:.1f} kWh  |  (buy: {energy_bought:.1f} MWh)\n")
+model.get_component("battery").var.useable_capacity
